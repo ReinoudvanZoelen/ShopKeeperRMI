f6d302c28bedf5ae35c194a7cede20ab401a16c3
ReinoudvanZoelen <f4n9yEBbdrIe5CTaRxdV>
2018-01-11 22:18:26 +0100

    Changed old content from models

--- src/_shared/Models/Bestelling.java
+++ src/_shared/Models/Bestelling.java
@@ -6,12 +6,21 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Bestelling implements Serializable {
+    public int id;
     public Date bestelMoment;
     public int BesteltijdInSeconden;
     public ArrayList<Product> producten;
     public Klant klant;
-    public int aantal;
     public boolean completed;
+
+    public Bestelling(int id, Date bestelMoment, int besteltijdInSeconden, ArrayList<Product> producten, Klant klant, boolean completed) {
+        this.id = id;
+        this.bestelMoment = bestelMoment;
+        BesteltijdInSeconden = besteltijdInSeconden;
+        this.producten = producten;
+        this.klant = klant;
+        this.completed = completed;
+    }
 
     public Bestelling(Klant klant, ArrayList<Product> producten) {
         this.bestelMoment = new Date();
@@ -28,7 +37,6 @@ public class Bestelling implements Serializable {
                 "bestelMoment=" + bestelMoment +
                 ", BesteltijdInSeconden=" + BesteltijdInSeconden +
                 ", klant=" + klant +
-                ", aantal=" + aantal +
                 ", completed=" + completed +
                 '}';
     }
