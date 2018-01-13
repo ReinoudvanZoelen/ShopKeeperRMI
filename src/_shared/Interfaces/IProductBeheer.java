3b7179ea20abd282e2df0666676df0a9162a0b88
Reinoud van Zoelen <reinoud95@gmail.com>
2018-01-13 16:46:39 +0100

    Changed ArrayList to compatible List

--- src/_shared/Interfaces/IProductBeheer.java
+++ src/_shared/Interfaces/IProductBeheer.java
@@ -6,11 +6,12 @@ import _shared.Models.Product;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.List;
 
 public interface IProductBeheer extends Remote {
-    ArrayList<Product> GetProducten() throws RemoteException;
+    List<Product> GetProducten() throws RemoteException;
 
-    ArrayList<Bestelling> GetOpenstaandeBestellingen() throws RemoteException;
+    List<Bestelling> GetOpenstaandeBestellingen() throws RemoteException;
 
     int GetProductVoorraad(Product product) throws RemoteException;
 
